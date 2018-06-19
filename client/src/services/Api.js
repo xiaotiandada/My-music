2f16eb54b1c0e06fafd2445fa4e1b5eb85fd2587
xiaotiandada <952822399@qq.com>
2018-06-19 13:40:26 +0800

    feat(修改部署请求地址): 修改服务的请求地址

--- client/src/services/Api.js
+++ client/src/services/Api.js
@@ -5,7 +5,8 @@ export default () => {
   return axios.create({
     // baseURL: `https://easy-mock.com/mock/5aed174fefe0537f00d5c957/`
     // baseURL: `http://localhost:8081/`,
-    baseURL: `http://127.0.0.1:8081/`,
+    // baseURL: `http://127.0.0.1:8081/`,
+    baseURL: `http://123.207.60.132:8081/`,
     headers: {
       Authorization: `Bearer ${store.state.token}`
     }
